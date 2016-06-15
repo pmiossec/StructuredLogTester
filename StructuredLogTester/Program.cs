d8db1ab098acd0a1a972d866bcb3e025487a11dc
Philippe Miossec <pmiossec@gmail.com>
2016-06-15 22:59:44 +0200

    Add some logs

--- StructuredLogTester/Program.cs
+++ StructuredLogTester/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,46 @@ namespace StructuredLogTester
 {
     class Program
     {
+        private static ILogger logger;
+
         static void Main(string[] args)
         {
+            InitializeSerilog();
+
+            AndLogAStruct();
+
+            AndLogAnObject();
+
+            Console.ReadLine();
+        }
+
+        private static void AndLogAStruct()
+        {
+            var log = "little cat";
+            logger.Information("This is a wonderful {log}", log);
+        }
+
+        private static void AndLogAnObject()
+        {
+            var log = new { Animal = "cat", Name = "astroph" };
+            logger.Information("This is a wonderful {log}", log);
+            logger.Information("This is a wonderful {@log}", log);
+
+            var fruit = new[] { "Apple", "Pear", "Orange" };
+            logger.Information("In my bowl I have {Fruit}", fruit);
+        }
+
+        private static void InitializeSerilog()
+        {
+            logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .MinimumLevel.Debug()
+                .Enrich.WithThreadId()
+                .Enrich.WithProperty("MyMetaProperty", "Oh! the beautiful value!")
+                .WriteTo.ColoredConsole()
+                .WriteTo.File(@".\myLogFile.txt")
+                //.WriteTo.EventLog("Serilog tests")
+                .CreateLogger();
         }
     }
 }
